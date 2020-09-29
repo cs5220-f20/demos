cf5c5239cadd26e9a5e8605e4f47861bca754b6c
David Bindel <bindel@cornell.edu>
2020-09-29 01:22:37 +0000

    Updated centroid demo.

--- centroid/README.md
+++ centroid/README.md
@@ -8,3 +8,15 @@ Three versions of a centroid computation among a million data points.  Try the f
 3.  Try to get the information from GCC about vectorization.  What do you observe?
 4.  Try some of the tricks mentioned in the code optimization lecture (aligning the memory,
     explicitly re-associating to reveal independent work, etc).  Can you make things much faster?
+
+## Updated centroid
+
+Since some time has elapsed since we started this example, let's bring it to a successful
+conclusion.  If we explicitly use the `-ffast-math` flag, we tell GCC that it is free to
+reassociate addition, and give it much more flexibility to reassociate things.  We still
+do better with the version that keeps two parallel sums simultaneously.  We include an
+example of the OpenMP SIMD directives to try to coax the compiler to vectorize the loop,
+but it does nothing that GCC isn't willing to do for us already (at least on
+the version installed on Debian Buster).  We've updated the code to include a
+"speed of light" computation based on the estimated per core memory bandwidth
+on this machine; the current version essentially reaches that speed of light.
--- centroid/centroid.c
+++ centroid/centroid.c
@@ -20,6 +20,21 @@ void centroid1(double* result, double* xy, int n)
     result[1] = y/n;
 }
 
+void centroid1a(double* restrict result, 
+                const double* restrict xy, int n)
+{
+    double x = 0.0;
+    double y = 0.0;
+    #pragma omp simd reduction(+:x) reduction(+:y)
+    for (int i = 0; i < n; ++i) {
+        x += xy[2*i+0];
+        y += xy[2*i+1];
+    }
+    result[0] = x/n;
+    result[1] = y/n;
+}
+
+
 void centroid2(double* result, double* xy, int n)
 {
     double x = 0.0;
@@ -44,6 +59,26 @@ void centroid3(double* result, double* xs, double* ys, int n)
     result[1] = y/n;
 }
 
+void centroid3a(double* restrict result,
+                const double* restrict xs,
+                const double* restrict ys,
+                int n)
+{
+    double x = 0.0;
+    double y = 0.0;
+
+    #pragma omp simd reduction(+:x)
+    for (int i = 0; i < n; ++i)
+        x += xs[i];
+
+    #pragma omp simd reduction(+:x)
+    for (int i = 0; i < n; ++i)
+        y += ys[i];
+
+    result[0] = x/n;
+    result[1] = y/n;
+}
+
 int main()
 {
     int n = 1000000;
@@ -55,19 +90,36 @@ int main()
     double t = omp_get_wtime();
     for (int k = 0; k < 100; ++k)
         centroid1(result, xy, n);
-    printf("%g (%g, %g)\n", (omp_get_wtime()-t)/100, result[0], result[1]);
+    printf("1:  %g (%g, %g)\n", (omp_get_wtime()-t)/100, result[0], result[1]);
+
+   // Timing first option
+    t = omp_get_wtime();
+    for (int k = 0; k < 100; ++k)
+        centroid1a(result, xy, n);
+    printf("1a: %g (%g, %g)\n", (omp_get_wtime()-t)/100, result[0], result[1]);
 
     // Timing second option
     t = omp_get_wtime();
     for (int k = 0; k < 100; ++k)
         centroid2(result, xy, n);
-    printf("%g (%g, %g)\n", (omp_get_wtime()-t)/100, result[0], result[1]);
+    printf("2:  %g (%g, %g)\n", (omp_get_wtime()-t)/100, result[0], result[1]);
 
     // Timing third option
     t = omp_get_wtime();
     for (int k = 0; k < 100; ++k)
         centroid3(result, xy, xy+n, n);
-    printf("%g (%g, %g)\n", (omp_get_wtime()-t)/100, result[0], result[1]);
+    printf("3:  %g (%g, %g)\n", (omp_get_wtime()-t)/100, result[0], result[1]);
+
+    // Timing third option
+    t = omp_get_wtime();
+    for (int k = 0; k < 100; ++k)
+        centroid3a(result, xy, xy+n, n);
+    printf("3a: %g (%g, %g)\n", (omp_get_wtime()-t)/100, result[0], result[1]);
+
+    // Compare "speed of light"
+    double mem_bw = 12.4e9;  // GB/s
+    double mem_read = 2*n*sizeof(double);
+    printf("Time to read data: %g\n", mem_read/mem_bw);
 
     free(xy);
 }
